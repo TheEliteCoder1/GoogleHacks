a45b5a937c73afee7b5a0af33f16ccd58b89f713
RaphTheCoder <74937838+TheEliteCoder1@users.noreply.github.com>
2023-05-31 11:54:12 -0400

    Update Rainbow.js

--- Rainbow.js
+++ Rainbow.js
@@ -3,12 +3,11 @@
 // Changes Colors to Rgb
 
 // Instructions:
-// 1) Click on gmail link using inspect
-// 2) Find div with id content', and double click to change it to 'mycontent'
-// 3) Paste script in console
+// 1) Find div with id 'content', click on it
+// 2) Paste script in console
 // Note: (not compatible with IE browsers)
 
-var speed = 100 // milisecs
+var speed = 100; // milisecs
 var rotatedeg = 0;
 var allDivs = document.getElementsByTagName('div');
 
@@ -18,16 +17,16 @@ setInterval(changeBackground, speed);
 
 function numberInRange(max, min) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-}
+};
 
 function program() {
   for (var i = 0; i < document.getElementsByTagName(`a`).length; i++) {
       for (var j = 0; j < 1000; j++) {
         document.getElementsByTagName(`a`)[i].style.color = `white`;
         document.getElementsByTagName(`a`)[i].style.backgroundColor = `rgba(${numberInRange(0, 255)}, ${numberInRange(0, 255)}, ${numberInRange(0, 255)}, ${numberInRange(0.5, 1)})`;
-    }
-  }
-}
+    };
+  };
+};
 
 function changeBackground() {
     if (rotatedeg < 360)
@@ -36,14 +35,12 @@ function changeBackground() {
     } else {
         rotatedeg = 0;
         rotateDeg();
-    }
+    };
     
 
-    for (var i = 0; i < allDivs.length; i++) {
-        allDivs[i].classList.add("grid-container")
-        for (var j = 0; j < allDivs.children.length; j++) {
-            allDivs.children[j].classList.add("grid-item")
-        }
-    }
-}
+   document.body.style.backgroundColor = `rgba(${numberInRange(0, 255)}, ${numberInRange(0, 255)}, ${numberInRange(0, 255)}, ${numberInRange(0.5, 1)})`;
+   for (var i = 0; i < document.body.children.length; i++) {
+       document.body.chlidren[i].style.transform = `rotate(${rotatedeg}deg);`;
+   }
+};
   
