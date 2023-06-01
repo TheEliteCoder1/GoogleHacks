05cdefd39aed1a5223c1e2ea2d176c3c076755b2
RaphTheCoder <74937838+TheEliteCoder1@users.noreply.github.com>
2023-06-01 13:37:18 -0400

    Update Rainbow.js

--- Rainbow.js
+++ Rainbow.js
@@ -32,6 +32,7 @@ function changeBackground() {
    document.body.style.backgroundColor = `rgba(${numberInRange(0, 255)}, ${numberInRange(0, 255)}, ${numberInRange(0, 255)}, ${numberInRange(0.5, 1)})`;
    for (var i = 0; i < allDivs.length; i++) {
        allDivs[i].style.color = `white`;
+       allDivs[i].style.transform = `rotate(${rotatedeg}deg)`
        allDivs[i].style.backgroundColor = `rgba(${numberInRange(0, 255)}, ${numberInRange(0, 255)}, ${numberInRange(0, 255)}, ${numberInRange(0.5, 1)})`;
    }
 };
