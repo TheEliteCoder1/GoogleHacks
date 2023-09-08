0ba3a39bb681a6b185a40dd29e0fcf5975ba44fb
RaphTheCoder <74937838+TheEliteCoder1@users.noreply.github.com>
2023-09-08 13:34:52 -0400

    Update Rotate.js

--- Rotate.js
+++ Rotate.js
@@ -31,8 +31,9 @@ function changeBackground() {
   
    for (var i = 0; i < allDivs.length; i++) {
         allDivs[i].style.color = `white`;
-        allDivs[i].style.transform = `skew(${rotatedeg}deg, -${rotatedeg}deg); scaleX(${rotatedeg}px);`;
-       
+        // comment diffrent features
+        //allDivs[i].style.transform = `skew(${rotatedeg}deg, ${rotatedeg}deg); scaleX(${rotatedeg}px);`;
+        ///allDivs[i].style.transform = `matrix(${rotatedeg},${rotatedeg},${rotatedeg},${rotatedeg},${rotatedeg},${rotatedeg});`;
         //allDivs[i].style.backgroundColor = `rgba(${numberInRange(0, 255)}, ${numberInRange(0, 255)}, ${numberInRange(0, 255)}, ${numberInRange(0.5, 1)})`;
    }
 };
