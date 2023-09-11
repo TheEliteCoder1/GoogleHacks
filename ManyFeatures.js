4f0bb6271166a1e49db0e959cd9bcb2ae19cda2a
RaphTheCoder <74937838+TheEliteCoder1@users.noreply.github.com>
2023-09-11 14:17:06 -0400

    Update ManyFeatures.js

--- ManyFeatures.js
+++ ManyFeatures.js
@@ -10,12 +10,34 @@
 var speed = 200; // milisecs
 var rotatedeg = 0;
 var allDivs = document.querySelectorAll('div');
+var randomColorLimit = 10;
 
 window.onload = setInterval(changeBackground, speed);
 
 
 function numberInRange(max, min) {
     return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// check for keyPress
+document.onkeypress = function (e) {
+    e = e || window.event;
+    var pressedKey = String.fromCharCode(e.keyCode)
+    if (pressedKey == "w") {
+        if (randomColorLimit < 255) {
+            randomColorLimit += 1
+        } else {
+            alert("You cannot have a randomColorLimit greater than 255.")
+        }
+    }
+    if (pressedKey == "s") {
+        if (randomColorLimit > 0) {
+            randomColorLimit -= 1
+        } else {
+            alert("You cannot have a randomColorLimit less than 0.")
+        }
+    }
+    
 };
 
 
@@ -32,9 +54,9 @@ function changeBackground() {
    for (var i = 0; i < allDivs.length; i++) {
         allDivs[i].style.color = `white`;
         // comment diffrent features
-        allDivs[i].style.transform = `rotate(${rotatedeg}deg)`;
+        //allDivs[i].style.transform = `rotate(${rotatedeg}deg)`;
         //allDivs[i].style.transform = `matrix(${rotatedeg},${rotatedeg},${rotatedeg},${rotatedeg},${rotatedeg},${rotatedeg})`;
-        //allDivs[i].style.backgroundColor = `rgba(${numberInRange(0, 255)}, ${numberInRange(0, 255)}, ${numberInRange(0, 255)}, ${numberInRange(0.5, 1)})`;
+        allDivs[i].style.backgroundColor = `rgba(${numberInRange(0, randomColorLimit)}, ${numberInRange(0, randomColorLimit)}, ${numberInRange(0, randomColorLimit)}, ${numberInRange(0.5, 1)})`;
    }
 };
   
